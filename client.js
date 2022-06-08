60833a8cbb4955d8fd2a2d837f67b4cb157f49b6
Siraj <sirajwahaj.ibrahim@gmail.com>
2022-06-08 22:24:51 +0000

    Included string to send name upon connection.

--- client.js
+++ client.js
@@ -2,17 +2,21 @@ const net = require("net");
 // establishes a connection with the game server
 const connect = function() {
   const conn = net.createConnection({
-    host: "",// IP address here,
-    port: "" // PORT number here,
+    host: "165.227.47.243",// IP address here,
+    port: 50541 // PORT number here,
   });
-
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
   conn.on("connect", () => { // code that does something when the connection is first established
     console.log("Champ is here!"); // Once connection is eastablished send message
-    
-  }); conn.on("data", (data) => { // Return the message from server when you get kicked out for idling
+  });
+
+  conn.on("connect", () => { // code that does something when the connection is first established
+    conn.write("Name: SWJ"); // Print out name to server
+  });
+  
+  conn.on("data", (data) => { // Return the message from server when you get kicked out for idling
     console.log("Server says: ", data);
   });
 
