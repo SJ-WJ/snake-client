1f927753aeb61f548fb686d7ddd5aff9666cb1bd
Siraj <sirajwahaj.ibrahim@gmail.com>
2022-06-08 23:18:14 +0000

    Experimenting with moving in different directions.

--- client.js
+++ client.js
@@ -10,10 +10,14 @@ const connect = function() {
 
   conn.on("connect", () => { // code that does something when the connection is first established
     console.log("Champ is here!"); // Once connection is eastablished send message
-  });
-
-  conn.on("connect", () => { // code that does something when the connection is first established
     conn.write("Name: SWJ"); // Print out name to server
+    conn.write("Move: up"); // Requesting server to move up, then experimenting with moving in different directions
+    setTimeout(() => {
+      conn.write("Move: up");
+    }, 500);
+    setInterval(() => {
+      conn.write("Move: left");
+    }, 500);
   });
   
   conn.on("data", (data) => { // Return the message from server when you get kicked out for idling
