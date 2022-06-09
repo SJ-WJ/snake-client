8ce66c4b84085f11a3865859fae8c084d8c26af8
Siraj <sirajwahaj.ibrahim@gmail.com>
2022-06-09 00:52:19 +0000

    Updated functions.

--- play.js
+++ play.js
@@ -1,25 +1,6 @@
 const connect = require("./client"); //Importing the connect function
+const setupInput = require("./input"); //Importing the setupInput function
 
 console.log("Connecting ...");
 connect();
-
-// setup interface to handle user input from stdin
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on("data", handleUserInput);
-  const handleUserInput = function() {
-    // your code here
-    stdin.on('data', (key) => {
-      if (key === '\u0003') { // \u0003 maps to ctrl+c input, terminate the game
-        process.exit();
-      }
-    });
-  };
-
-  return stdin;
-};
-
+setupInput();
