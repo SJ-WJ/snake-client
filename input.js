cb6f16dc2e38c0f788c099b1d0deb88016c6f72b
Siraj <sirajwahaj.ibrahim@gmail.com>
2022-06-09 06:08:55 +0000

    Addition of movement commands

--- input.js
+++ input.js
@@ -1,21 +1,33 @@
-// setup interface to handle user input from stdin
-const setupInput = function() {
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
+  stdin.on("data", handleUserInput);
   stdin.resume();
-
-  const handleUserInput = function() {
-    // your code here
-    stdin.on('data', (key) => {
-      if (key === '\u0003') { // \u0003 maps to ctrl+c input, terminate the game
-        process.exit();
-      }
-    });
-  };
-  
-  stdin.on("data", handleUserInput); //At this point were using it => In respone to "ReferenceError: Cannot access 'handleUserInput' before initialization"
   return stdin;
+};
+
+
+const handleUserInput = function(key) {
+  // your code here
+  if (key === '\u0003') { // \u0003 maps to ctrl+c input, terminate the game
+    process.exit();
+  }
+  if (key === 'w') { // "W, moving up"
+    connection.write("Move: up");
+  }
+  if (key === 'a') { // "A, moving left"
+    connection.write("Move: left");
+  }
+  if (key === 's') { // "S, moving down"
+    connection.write("Move: down");
+  }
+  if (key === 'd') { // "D, moving right"
+    connection.write("Move: right");
+  }
 };
 
 module.exports = setupInput;
